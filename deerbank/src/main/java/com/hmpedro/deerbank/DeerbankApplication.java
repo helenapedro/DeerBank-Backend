7721e9f101296e534d46850efa877bf3e869a363
Helena Pedro <mbeua94@gmail.com>
2025-11-29 18:33:24 -0600

    project setup

---
+++ deerbank/src/main/java/com/hmpedro/deerbank/DeerbankApplication.java
@@ -0,0 +1,13 @@
+package com.hmpedro.deerbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeerbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeerbankApplication.class, args);
+    }
+
+}
---
+++ deerbank/src/test/java/com/hmpedro/deerbank/DeerbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmpedro.deerbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeerbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
