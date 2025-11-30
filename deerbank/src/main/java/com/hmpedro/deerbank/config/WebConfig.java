eea251adbb5d89d58544a0a99fd7c2f227e10143
Helena Pedro <mbeua94@gmail.com>
2025-11-30 17:48:51 -0600

    Create WebConfig fole

---
+++ deerbank/src/main/java/com/hmpedro/deerbank/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.hmpedro.deerbank.config;
+
+import lombok.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:8081",
+                        "https://iowadeerbank.hmpedro.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
